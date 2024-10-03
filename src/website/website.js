333cf80ee21cad33230f1f1f2f0c9117bf98ded1
SyedZainulAbideen494 <zainkaleem27@gmail.com>
2024-10-03 14:01:17 +0530

    Initial commit

--- src/website/website.js
+++ src/website/website.js
@@ -81,19 +81,22 @@ const DownloadPage = () => {
         </header>
 
         <section className="hero">
-          <div className="hero-content">
-            <h1>Transform Your Study Routine</h1>
-            <p>Maximize your productivity and achieve your goals with the ultimate study tool.</p>
-            <div className="hero-buttons">
-              <button className="cssbuttons-io-button" onClick={openModal}>Get started
-                <div className="icon">
-                  <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor"></path></svg>
-                </div>
-              </button>
-              
-            </div>
-          </div>
-        </section>
+  <div className="hero-content">
+    <h1>Transform Your Study Routine</h1>
+    <p className="user-count">Join 50,000+ learners who are transforming their study routines with Eduisfy!</p>
+
+    <div className="hero-buttons">
+      <button className="cssbuttons-io-button" onClick={openModal}>Get started
+        <div className="icon">
+          <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor"></path>
+          </svg>
+        </div>
+      </button>
+    </div>
+  </div>
+</section>
 
         <section id="features">
           <FeaturesOverview />
