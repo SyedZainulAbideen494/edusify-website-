bf275461252c74c68f058b937765b4132f3d501f
SyedZainulAbideen494 <zainkaleem27@gmail.com>
2024-10-09 12:08:31 +0530

    m

--- src/website/students-testimonials.js
+++ src/website/students-testimonials.js
@@ -36,7 +36,7 @@ const TestimonialHero = () => {
           <div className="col-12__testimonial__hero__section">
             <div className="hero-content__testimonial__hero__section">
               <h1 className="hero-title__testimonial__hero__section">Transform Your Study Routine</h1>
-              <p className="hero-description__testimonial__hero__section">Your all-in-one study companion designed to enhance learning, streamline organization, and foster collaboration among students.</p>
+              <p className="hero-description__testimonial__hero__section">Your all-in-one study app with AI tools and everything students need to learn, stay organized, and succeed.</p>
               <div className="user-stats__testimonial__hero__section">
                 <img src="https://scontent.cdninstagram.com/v/t51.2885-19/460953738_2007858242965734_3101439078776234228_n.jpg?stp=dst-jpg_s100x100&_nc_cat=109&ccb=1-7&_nc_sid=bf7eb4&_nc_ohc=fk2uBmF9QlEQ7kNvgG2Btve&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.cdninstagram.com&oh=00_AYC9E2PYHezvsQbYUM3xNF9UGCpxtNhdkA2gNHTMUxX_CQ&oe=670B0E02" alt="User 1" className="user-icon__testimonial__hero__section" />
                 <img src="https://scontent.cdninstagram.com/v/t51.2885-19/459309825_897228809068380_4178068191224473134_n.jpg?stp=dst-jpg_s160x160&_nc_cat=107&ccb=1-7&_nc_sid=f7ccc5&_nc_ohc=nb5tics5Xa0Q7kNvgG0ZLm9&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.cdninstagram.com&oh=00_AYB0cGVLmY7AJb01UJA4jkzS0JV7oVOku0lIuAlBOrXHBg&oe=670B1011" alt="User 2" className="user-icon__testimonial__hero__section" />
@@ -49,7 +49,7 @@ const TestimonialHero = () => {
       </div>
               </div>
               <div className="hero-buttons">
-      <button className="cssbuttons-io-button" onClick={openModal}>Get started
+      <button className="cssbuttons-io-button" onClick={openModal}>Get started - it's free!
         <div className="icon">
           <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path d="M0 0h24v24H0z" fill="none"></path>
