2e5d81014e34978233e2e2f76b84524b35185956
SyedZainulAbideen494 <zainkaleem27@gmail.com>
2025-01-09 18:57:52 +0530

    m

--- src/components/fetaures.js
+++ src/components/fetaures.js
@@ -9,6 +9,7 @@ import featureImage4 from '../images/fetaures-images/4.png';
 import featureImage5 from '../images/fetaures-images/5.png';
 import featureImage6 from '../images/fetaures-images/6.png';
 import featureImage7 from '../images/fetaures-images/7.png';
+import zain from '../images/photo_2_2025-01-07_23-47-34.jpg'
 
 
 const FeaturesSection = () => {
@@ -23,6 +24,7 @@ const FeaturesSection = () => {
         <img src={featureImage5} alt="Feature 3" className="feature-image" />
         <img src={featureImage6} alt="Feature 3" className="feature-image" />
         <img src={featureImage7} alt="Feature 3" className="feature-image" />
+        <img src={zain} alt="Feature 3" className="feature-image" style={{display: 'none'}}/>
       </div>
     </section>
   );
