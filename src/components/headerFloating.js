d1bc37b5fcc7d3c68092664eaec504a2c75991c4
Syed Zain ul Abideen <syedzainulabideen@Syeds-MacBook-Air.local>
2025-04-27 17:05:45 +0530

    m

--- src/components/headerFloating.js
+++ src/components/headerFloating.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './headerFloating.css'; // Import the CSS file
 import logo from '../images/1722866972968-removebg-preview.png';
+import { Link } from 'react-router-dom';
 
 const NavigationBar = () => {
   return (
@@ -8,7 +9,9 @@ const NavigationBar = () => {
       <div className="nav__logo">
         <img src={logo} alt="EduSify Logo" />
       </div>
+                <Link to="/get-app" style={{ textDecoration: 'none' }}>
       <button className="nav__btn">Get App</button>
+      </Link>
     </header>
   );
 };
